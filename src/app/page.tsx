ac3a03f66693501de2d1895baee8179c71d255d4
Romaric Zounlome <rzounlom@gmail.com>
2023-10-13 10:29:48 -0400

    add fuction to shorten url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,31 @@
 import Body from "./components/Body";
-import Input from "./components/Input";
-import InfoBox from "./components/InfoBox";
 import Footer from "./components/Footer";
+import InfoBox from "./components/InfoBox";
+import Input from "./components/Input";
 import Nav from "./components/Nav";
 
-async function short() {
-  const url = {
-    url: "https://www.linkein.com/company/frontend-mentor",
-  };
-  const data = await fetch(`https://shrtlnk.dev/api/v2/link`, {
-    method: "POST",
-    headers: {
-      "api-key": `${process.env.SHORT_API}`,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "Content-Length": `${url.url.length}`,
-    },
-    cache: "force-cache",
-    body: JSON.stringify(url),
-  });
-  const response = await data.json();
-  console.log(response);
+async function shortenUrl(url: string) {
+  try {
+    const data = await fetch("https://api.rebrandly.com/v1/links", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: `${process.env.REBRANDLY_API_KEY}}`,
+      },
+      body: JSON.stringify({
+        destination: url,
+      }),
+    });
+
+    const newUrl = await data.json();
+    console.log("NEW URL", newUrl);
+    return newUrl;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default async function Home() {
-  short();
   return (
     <main className=" min-h-fit overflow-auto">
       <Nav />
--- src/pages/api/short.ts
+++ src/pages/api/short.ts
@@ -4,27 +4,33 @@ type ResponseData = {
   message: string;
 };
 
-export default async function handler(
+export default async function shortenUrl(
   req: NextApiRequest,
   usePonyFill = true,
   res: NextApiResponse<ResponseData>
 ) {
-  const url = {
-    url: "https://www.linkein.com/company/frontend-mentor",
-  };
-  const data = await fetch(`https://shrtlnk.dev/api/v2/link`, {
-    method: "POST",
-    headers: {
-      "api-key": `${process.env.SHORT_API}`,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    cache: "force-cache",
-    body: JSON.stringify(url),
-  });
-  const response = await data.json();
-  console.log(response);
+  console.log(req.body);
+  // try {
+  //   const url = {
+  //     url: "https://www.linkein.com/company/frontend-mentor",
+  //   };
+  //   const data = await fetch(`https://shrtlnk.dev/api/v2/link`, {
+  //     method: "POST",
+  //     mode: "cors",
+  //     headers: {
+  //       "api-key": `${process.env.SHORT_API}`,
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     cache: "no-cache",
+  //     body: JSON.stringify(url),
+  //   });
+
+  //   const response = await data.json();
+  //   console.log(response);
+  // } catch (error) {
+  //   console.log(error);
+  // }
+
   res.status(200).json({ message: "Hello from Next.js!" });
 }
-
-//
